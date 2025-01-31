f1be65010649565654d2aa63c281169bedbacfd0
tmneth <maksimb@wix.com>
2025-01-31 20:16:05 +0200

    breaking(hi): hi

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Reffact</h1>
+      <h1>Vite + Refsdafdsffact</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
